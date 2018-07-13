7b6ead7eed66371d0788bbdf873274d1d74114f9
Lonca Emmanuel <emmanuel.lonca@univ-artois.fr>
2018-07-13 13:51:15 +0200

    set maven default directory architecture for unit tests

--- fr.cril.rubens.generator/src/main/java/fr.cril.rubens.generator/fr/cril/rubens/generator/Generator.java
+++ fr.cril.rubens.generator/src/main/java/fr.cril.rubens.generator/fr/cril/rubens/generator/Generator.java
@@ -3,8 +3,13 @@ package fr.cril.rubens.generator;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +33,8 @@ public class Generator {
 	private int statusCode = 0;
 
 	private final GeneratorOptionsReader generatorOptions;
+	
+	private boolean cleanedOldFiles = false;
 
 	/**
 	 * Launches the generator. See documentation for command line arguments.
@@ -86,17 +93,42 @@ public class Generator {
 		if(this.statusCode != 0) {
 			return;
 		}
+		final Collection<String> extensions = instance.getFileExtensions();
+		final File outputDirectory = this.generatorOptions.getOutputDirectory();
+		cleanOldFiles(outputDirectory, extensions);
 		try {
-			final Collection<String> extensions = instance.getFileExtensions();
 			for(final String ext : extensions) {
-				instance.write(ext, new FileOutputStream(new File(this.generatorOptions.getOutputDirectory(), instanceCount+ext)));
+				instance.write(ext, new FileOutputStream(new File(outputDirectory, instanceCount+ext)));
 			}
 			this.instanceCount++;
 		} catch (IOException e) {
 			this.statusCode = STATUS_IO_EXCEPTION_DURING_GENERATION;
 		}
 	}
+
+	private void cleanOldFiles(final File outputDirectory, final Collection<String> extensions) {
+		if(!this.cleanedOldFiles) {
+			this.cleanedOldFiles = true;
+			for(final Path path : Arrays.stream(outputDirectory.listFiles(File::isFile)).map(File::toURI).map(Paths::get).collect(Collectors.toList())) {
+				if(extensions.stream().anyMatch(path.toAbsolutePath()::endsWith)) {
+					try {
+						Files.delete(path);
+					} catch (IOException e) {
+						LOGGER.warn("cannot clean existing file {}; reason is \"{}\"", path.toAbsolutePath(), e.getMessage());
+					}
+				}
+			}
+		}
+	}
 	
+	/**
+	 * Returns the status code of the generation process after {@link Generator#generate()} has been called.
+	 * 
+	 * This code is the one returned at exit time when calling {@link Generator#main(String[])}.
+	 * A non-zero value indicates an error occurred.
+	 * 
+	 * @return the status code of the generation process
+	 */
 	public int getStatusCode() {
 		return this.statusCode;
 	}
