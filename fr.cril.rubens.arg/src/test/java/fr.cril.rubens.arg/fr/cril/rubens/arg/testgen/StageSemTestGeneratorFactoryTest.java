f0f688afb1d29f154438723a1e2961abdaaad2cc
Lonca Emmanuel <emmanuel.lonca@univ-artois.fr>
2018-11-06 14:55:36 +0100

    added factories for new test generators

---
+++ fr.cril.rubens.arg/src/test/java/fr.cril.rubens.arg/fr/cril/rubens/arg/testgen/StageSemTestGeneratorFactoryTest.java
@@ -0,0 +1,29 @@
+package fr.cril.rubens.arg.testgen;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import fr.cril.rubens.arg.core.ArgumentationFramework;
+import fr.cril.rubens.specs.InstanceTranslator;
+
+public class StageSemTestGeneratorFactoryTest {
+	
+	@Test
+	public void testInitInstance() {
+		assertEquals(new ArgumentationFramework(), new StageSemTestGeneratorFactory().initInstance());
+	}
+	
+	@Test
+	public void testTranslators() {
+		final List<InstanceTranslator<ArgumentationFramework>> translators = new StageSemTestGeneratorFactory().translators();
+		assertEquals(2, translators.size());
+		assertTrue(translators.stream().anyMatch(t -> t.getClass().equals(fr.cril.rubens.arg.testgen.NewArgTranslator.class)));
+		assertEquals(1, translators.stream().filter(t -> t.getClass().equals(NewAttackTranslator.class)).map(t -> (NewAttackTranslator) t).map(NewAttackTranslator::getExtensionSetComputer)
+				.filter(c -> c == EExtensionSetComputer.STAGE_SEM).count());
+	}
+
+}
