0f078b3895bb6c9042b60c37ebfc626315dd80c2
Lonca Emmanuel <emmanuel.lonca@univ-artois.fr>
2018-11-12 12:48:08 +0100

    added a checker for ICCMA17

---
+++ fr.cril.rubens.arg/src/main/java/fr.cril.rubens.arg/fr/cril/rubens/arg/checking/checkers/ICCMA2017Checker.java
@@ -0,0 +1,30 @@
+package fr.cril.rubens.arg.checking.checkers;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import fr.cril.rubens.arg.core.ArgumentationFramework;
+import fr.cril.rubens.specs.ACheckerFactoryCollection;
+import fr.cril.rubens.specs.ReflectorParam;
+
+/**
+ * A checker factory collection matching the regular tracks of ICCMA 2017.
+ * 
+ * These tracks consist of the Cartesian product of the queries (all extensions, one extension, credulous acceptance, skeptical acceptance)
+ * and the semantics (complete, grounded, preferred, stable, semistable, stage and ideal) involved in the competition.
+ * 
+ * Note that contrary to the competition, some equivalent tasks are launched more than once (e.g. SE-GR and EE-GR).
+ * 
+ * @author Emmanuel Lonca - lonca@cril.fr
+ */
+@ReflectorParam(name="ICCMA2017")
+public class ICCMA2017Checker extends ACheckerFactoryCollection<ArgumentationFramework> {
+	
+	/**
+	 * Builds a new instance of {@link ICCMA2017Checker}.
+	 */
+	public ICCMA2017Checker() {
+		super(Stream.of("CO", "GR", "PR", "ST", "SST", "STG", "ID").map(sem -> Stream.of("EE", "SE", "DS", "DC").map(q -> q+"-"+sem)).flatMap(s -> s).collect(Collectors.toList()));
+	}
+
+}
