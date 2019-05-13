cf07bed13f52d443e9958867cc2ba56e440adaad
Emmanuel Lonca <lonca@cril.fr>
2019-05-13 13:37:18 +0200

    added license on source files

--- fr.cril.rubens.arg/src/main/java/fr.cril.rubens.arg/fr/cril/rubens/arg/testgen/AttackRemovalTranslator.java
+++ fr.cril.rubens.arg/src/main/java/fr.cril.rubens.arg/fr/cril/rubens/arg/testgen/AttackRemovalTranslator.java
@@ -1,5 +1,27 @@
 package fr.cril.rubens.arg.testgen;
 
+/*-
+ * #%L
+ * RUBENS module for abstract argumention problems
+ * %%
+ * Copyright (C) 2019 Centre de Recherche en Informatique de Lens (CRIL) — Artois University and CNRS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import java.util.Random;
 
 import fr.cril.rubens.arg.core.ArgumentSet;
